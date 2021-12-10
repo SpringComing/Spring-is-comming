b2ec3016b2ec32bd1e22d34fd4b5b3c4e6ad4113
eogkdfh <eogkdfh@gmail.com>
2021-12-10 16:23:29 +0900

    backend prototype

---
+++ backend/springcome/src/main/java/springcome/controller/MainController.java
@@ -0,0 +1,20 @@
+package springcome.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class MainController {
+
+	
+	@RequestMapping({"","/main"})
+	public String index() {
+    	return "index";
+    }
+	
+
+}
---
+++ backend/springcome/src/main/java/springcome/controller/api/ProjectController.java
@@ -0,0 +1,10 @@
+package springcome.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("ProjectApiController")
+@RequestMapping("/project/api")
+public class ProjectController {
+	
+}
