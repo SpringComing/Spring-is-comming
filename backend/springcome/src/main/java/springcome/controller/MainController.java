3d90cb914b640c58a7461f0ecb7b2222ed5ad88c
hyeon <hyeon2142@naver.com>
2021-12-13 12:10:54 +0900

    springex -> springcome 주석 작성

--- backend/springcome/src/main/java/springcome/controller/MainController.java
+++ backend/springcome/src/main/java/springcome/controller/MainController.java
@@ -1,20 +1,57 @@
 package springcome.controller;
 
 
-
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import springcome.auth.PrincipalDetails;
+import springcome.vo.UserVo;
 
-
-@Controller
+@Controller // View를 리턴
 public class MainController {
-
 	
-	@RequestMapping({"","/main"})
-	public String index() {
-    	return "index";
-    }
-	
+	@RequestMapping({"", "/main"})
+	public String index(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
+		
+		UserVo vo = new UserVo();
+		vo.setEmail(principalDetails.getEmail());
+		vo.setJoin_date(principalDetails.getJoindate());
+		vo.setBirth(principalDetails.getBirth());
+		vo.setName(principalDetails.getUsername());
+		
 
+		
+		model.addAttribute("UserVo",vo);
+		
+		return "index";
+	}
+	
+	@RequestMapping({"/user"})
+	public @ResponseBody String user() {
+		return "user";
+	}
+	
+	@RequestMapping({"/joinForm"})
+	public String joinForm() {
+		return "joinForm";
+	}
+	
+	@RequestMapping({"/loginForm"})
+	public String loginForm() {
+		return "loginForm";
+	}
+	
+	@RequestMapping({"/findForm"})
+	public String findForm() {
+		return "findForm";
+	}
+	
+	@RequestMapping({"/test"})
+	public String testForm() {
+		return "test";
+	}
+	
 }
---
+++ backend/springcome/src/main/java/springcome/service/UserService.java
@@ -0,0 +1,42 @@
+package springcome.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import springcome.repository.UserRepository;
+import springcome.vo.UserVo;
+
+
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+
+	public boolean join(UserVo vo) {
+		return userRepository.join(vo);
+	}
+
+	public UserVo findByUseremail(String useremail) {
+		return userRepository.findByUseremail(useremail);
+	}
+	
+	public UserVo findEmail(String name, String tel) {
+		return userRepository.findEmail(name, tel);
+	}
+	
+	public UserVo findPassword(String email, String tel) {
+		return userRepository.findPassword(email, tel);
+	}
+	
+	public UserVo findTel(String tel) {
+		return userRepository.findTel(tel);
+	}
+	
+	public int updatePWD(String email, String password) {
+
+		return userRepository.updatePWD(email, password);
+
+	}
+
+}
